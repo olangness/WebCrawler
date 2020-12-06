1046ade39f93d2181a8a396986bffe30a8c05a7b
DrewWick <45107135+DrewWick@users.noreply.github.com>
2020-12-06 13:10:31 -0600

    adjusted implementation from github
    link:
    https://gist.github.com/CodeCommissions/43a5aac117873dabcb1c9bdbf98cbd39

--- WebCrawler/MainWindow.xaml.cs
+++ WebCrawler/MainWindow.xaml.cs
@@ -1,9 +1,22 @@
-﻿using System;
+﻿using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using AngleSharp.Text;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -15,14 +28,86 @@ using System.Windows.Shapes;
 
 namespace WebCrawler
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private string Title { get; set; }
+        private string Url { get; set; }
+        //private string siteUrl = "";
+        //public string[] QueryTerms { get; } = { "", "" };
+        public List<String> QueryTerms { get; set; }
+
+        String URL;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ScrapeWebsite();
+        }
+        internal async void ScrapeWebsite()
+        {
+            URL = textBox1.Text;
+            CancellationTokenSource cancellationToken = new CancellationTokenSource();
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage request = await httpClient.GetAsync(URL);
+            cancellationToken.Token.ThrowIfCancellationRequested();
+
+            Stream response = await request.Content.ReadAsStreamAsync();
+            cancellationToken.Token.ThrowIfCancellationRequested();
+
+            HtmlParser parser = new HtmlParser();
+            IHtmlDocument document = parser.ParseDocument(response);
+
+            GetScrapeResults(document);
+        }
+        private void GetScrapeResults(IHtmlDocument document)
+        {
+            IEnumerable<IElement> articleLink;
+
+            foreach (var term in QueryTerms)
+            {
+                articleLink = document.All.Where(x => x.ClassName == "views-field views-field-nothing" &&
+                (x.ParentElement.InnerHtml.Contains(term) || x.ParentElement.InnerHtml.Contains(term.ToLower())));
+                if (articleLink.Any())
+                {
+                    PrintResults(articleLink);
+                }
+            }
+        }
+
+        public void PrintResults(IEnumerable<IElement> articleLink)
+        {
+            foreach (var element in articleLink)
+            {
+                CleanUpResults(element);
+                dataGridView1.DataContext = $"{Title} - {Url}{Environment.NewLine}";
+            }
+        }
+
+        private void CleanUpResults(IElement result)
+        {
+            string htmlResult = result.InnerHtml.ReplaceFirst("        <span class=\"field-content\"><div><a href=\"", "https://www.oceannetworks.ca");
+            htmlResult = htmlResult.ReplaceFirst("\">", "*");
+            htmlResult = htmlResult.ReplaceFirst("</a></div>\n<div class=\"article-title-top\">", "-");
+            htmlResult = htmlResult.ReplaceFirst("</div>\n<hr></span>  ", "");
+
+            SplitResults(htmlResult);
+        }
+
+        private void SplitResults(string htmlResult)
+        {
+            string[] splitResults = htmlResult.Split('*');
+            Url = splitResults[0];
+            Title = splitResults[1];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //topics.Add(textBox2.Text);
+            QueryTerms.Add(textBox2.Text);
         }
     }
 }
