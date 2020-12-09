e1064e1931e8e53ec2373463adc32e2af98983cd
damien-danelson <64103237+damien-danelson@users.noreply.github.com>
2020-12-09 16:22:21 -0600

    Log, Link, and Topic files
    Added Log, Link, and Topic files with storage capabilities

---
+++ WebCrawler/Model/Data Storage/Log.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Log
+    {
+        string messageText;
+        DateTime messageTime;
+
+
+        public Log() { }
+
+        public Log(string message, DateTime time)
+        {
+            MessageText = message;
+            
+            MessageTime = time;
+        }
+
+        public string MessageText
+        {
+            get { return messageText; }
+
+            set { messageText = value; }
+        }
+
+        public DateTime MessageTime
+        {
+            get { return messageTime; }
+
+            set { messageTime = value; }
+        }
+    }
+}
